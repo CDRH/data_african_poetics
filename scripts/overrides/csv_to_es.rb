eb0a134508cd8c2fa5b0e8d292d01e22ba1b97dc
William Dewey <wkdewey@gmail.com>
2022-07-20 09:20:45 -0500

    move helper method into csvtoes

---
+++ scripts/overrides/csv_to_es.rb
@@ -0,0 +1,11 @@
+class CsvToEs
+  def get_value(name, parse=false)
+    if @row[name] && @row[name].length > 0
+      if parse
+        JSON.parse(@row[name])
+      else
+        @row[name]
+      end
+    end
+  end
+end
--- scripts/overrides/csv_to_es_news.rb
+++ scripts/overrides/csv_to_es_news.rb
@@ -93,16 +93,6 @@ class CsvToEsNews < CsvToEs
     result
   end
 
-  def get_value(name, parse=false)
-    if @row[name] && @row[name].length > 0
-      if parse
-        JSON.parse(@row[name])
-      else
-        @row[name]
-      end
-    end
-  end
-
   def date_not_after
     if @row["Source access date"]
       Datura::Helpers.date_standardize(@row["Source access date"], false)
