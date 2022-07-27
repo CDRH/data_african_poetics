31d197c340dc04c9f48a64d169be1c93295615e3
William Dewey <wkdewey@gmail.com>
2022-07-27 13:40:13 -0500

    update overrides for events based on new airtable

--- scripts/overrides/csv_to_es_events.rb
+++ scripts/overrides/csv_to_es_events.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class CsvToEsEvents < CsvToEs
   def id
     get_id
@@ -34,4 +35,59 @@ class CsvToEsEvents < CsvToEs
   def type
     get_value("Event type")
   end
+
+  def person
+    people = []
+    nonpoets = get_value("person-notpoet", true)
+    poets = get_value("person-poet", true)
+    if nonpoets && nonpoets.length > 0
+      nonpoets.each do |nonpoet|
+        name = /\[(.*)\]/.match(nonpoet)[1] if /\[(.*)\]/.match(nonpoet)
+        people << { name: name }
+      end
+    end
+    if poets && poets.length > 0
+      poets.each do |poet|
+        name = /\[(.*)\]/.match(poet)[1] if /\[(.*)\]/.match(poet)
+        id = /\((.*)\)/.match(poet)[1] if /\((.*)\)/.match(poet)
+        role = "African Poet"
+        people << { name: name, role: role, id: id }
+      end
+    end
+    people
+  end
+
+  def topics
+    get_value("topics-decade")
+  end
+
+  def places
+    get_value("places")
+  end
+
+  def spatial
+    locations = []
+    if get_value("spatial.country")
+      JSON.parse(get_value("spatial.country")).each do |country|
+        location = { "country" => JSON.parse(get_value("spatial.country")) }
+        if get_value("spatial.city")
+          location["city"] = JSON.parse(get_value("spatial.city"))
+        end
+        if get_value("spatial.region")
+          location["region"] = JSON.parse(get_value("spatial.region"))
+        end
+        locations << location
+      end
+    end
+    locations
+  end
+
+  def relation
+    get_value("commentaries_relation")
+  end
+
+  def medium
+    get_value("news_items")
+  end
+
 end
