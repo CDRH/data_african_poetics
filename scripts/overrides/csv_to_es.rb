669ae0ad0b3cefbd4e7c24a8cb257809239d7718
William Dewey <wkdewey@gmail.com>
2023-02-20 14:48:21 -0600

    index all fields into text field for more search results

--- scripts/overrides/csv_to_es.rb
+++ scripts/overrides/csv_to_es.rb
@@ -1,4 +1,9 @@
 class CsvToEs
+
+  def array_to_string (array,sep)
+    return array.map { |i| i.to_s }.join(sep)
+  end
+  
   def get_value(name, parse=false)
     if @row[name] && @row[name].length > 0
       if parse
@@ -10,5 +15,13 @@ class CsvToEs
   end
 
   def abstract
+  end
+
+  def text
+    built_text = []
+    @row.each do |column_name, value|
+      built_text << value.to_s
+    end
+    return array_to_string(built_text, " ")
   end
 end
